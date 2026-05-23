b58ab1f7857a2f4e9b46ddbb37245519105ea547
Rahul <sahrahul04644@gmail.com>
2026-05-23 12:43:54 +0530

    fixed something

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
   'https://pocketfriendlyofficial.netlify.app',
   'http://localhost:3000',
   'http://localhost:5500',
-  'http://127.0.0.1:5500/frontend/index.html',
+  'http://127.0.0.1:5500',
   'http://localhost:8080',
 ];
 
